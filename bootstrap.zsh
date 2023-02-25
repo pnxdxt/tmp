0861fe59b33d9de96cf22722a7fba37ed00ccb08
pnxdxt <paul.nodet@gmail.com>
2023-02-25 19:04:20 +0100

    update script

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -6,22 +6,10 @@ if ! [[ "$OSTYPE" =~ darwin* ]]; then
   exit
 fi
 
-# Keep-alive: update existing sudo time stamp until the script has finished
-if test ! "$(command -v sudo)"; then
-  echo_warn "The Script Require Root Access. Please Enter Your Password."
-  sudo -v
-  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-  echo_ok "Done!"
-fi
-
-echo "Hello world"
-cd ~ || exit 1
-
-declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/pnxdxt/tmp/main/utils.zsh"
-
 download() {
 	local url="$1"
 	local output="$2"
+	echo url: "$url"
 	if command -v "curl" &> /dev/null; then
 		curl \
 			--location \
@@ -43,21 +31,76 @@ download() {
 	return 1
 }
 
+declare -r GITHUB_REPOSITORY="pnxdxt/tmp"
+declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/main/utils.zsh"
+declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/main"
+declare dotfilesDirectory="$HOME/.dotfiles"
+declare skipQuestions=false
 
-download_utils() {
-	local tmpFile="$(mktemp /tmp/XXXXX)"
-	download "$DOTFILES_UTILS_URL" "$tmpFile" \
-			&& . "$tmpFile" \
-			&& rm -rf "$tmpFile" \
-			&& return 0
+main() {
+	local tmpFile=""
 
-	return 1
+	# Load utils
+	cd ~ || exit 1
+	tmpFile="$(mktemp /tmp/XXXXX)"
+	echo "tmpFile: $tmpFile"
+	download "$DOTFILES_UTILS_URL" "$tmpFile" && . "$tmpFile" && rm -rf "$tmpFile"
+
+	# Keep-alive: update existing sudo time stamp until the script has finished
+	ask_for_sudo
+
+	# download dotfiles
+	echo_warn "• Download and extract archive\n\n"
+	tmpFile="$(mktemp /tmp/XXXXX)"
+	download "$DOTFILES_TARBALL_URL" "$tmpFile"
+	print_result $? "Download archive" "true"
+	printf "\n"
+
+	skip_questions "$@" && skipQuestions=true
+
+	if ! $skipQuestions; then
+		ask_for_confirmation "Do you want to store the dotfiles in '$dotfilesDirectory'?"
+
+		if ! answer_is_yes; then
+			dotfilesDirectory=""
+			while [ -z "$dotfilesDirectory" ]; do
+				ask "Please specify another location for the dotfiles (path): "
+				dotfilesDirectory="$(get_answer)"
+			done
+		fi
+
+		# Ensure the `dotfiles` directory is available
+		while [ -e "$dotfilesDirectory" ]; do
+			ask_for_confirmation "'$dotfilesDirectory' already exists, do you want to overwrite it?"
+			if answer_is_yes; then
+				rm -rf "$dotfilesDirectory"
+				break
+			else
+				dotfilesDirectory=""
+				while [ -z "$dotfilesDirectory" ]; do
+					ask "Please specify another location for the dotfiles (path): "
+					dotfilesDirectory="$(get_answer)"
+				done
+			fi
+		done
+
+		printf "\n"
+    else
+			rm -rf "$dotfilesDirectory" &> /dev/null
+    fi
+
+    mkdir -p "$dotfilesDirectory"
+    print_result $? "Create '$dotfilesDirectory'" "true"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # Extract archive in the `dotfiles` directory.
+    extract "$tmpFile" "$dotfilesDirectory"
+    print_result $? "Extract archive" "true"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    rm -rf "$tmpFile"
+    print_result $? "Remove archive"
 }
 
-# Load utils
-if [ -x "utils.sh" ]; then
-	echo "utils.sh exists"
-	. "utils.sh" || exit 1
-else
-	download_utils || exit 1
-fi
+main "$@"
+echo "Done"
--- utils.zsh
+++ utils.zsh
@@ -1,6 +1,49 @@
 # helpers
-function echo_ok { echo '\033[1;32m'"$1"'\033[0m'; }
-function echo_warn { echo '\033[1;33m'"$1"'\033[0m'; }
-function echo_error { echo '\033[1;31mERROR: '"$1"'\033[0m'; }
+echo_ok() { echo '\033[1;32m'"$1"'\033[0m'; }
+echo_warn() { echo '\033[1;33m'"$1"'\033[0m'; }
+echo_error() { echo '\033[1;31mERROR: '"$1"'\033[0m'; }
 
-echo_ok "Hello world"
+print_result() {
+	if [ "$1" -eq 0 ]; then
+		echo_ok "✓ $2"
+	else
+		echo_error "$2"
+		[ "$3" == "true" ] && exit 1
+	fi
+}
+
+ask_for_sudo() {
+	if test ! "$(command -v sudo)"; then
+		echo "The Script Require Root Access. Please Enter Your Password."
+    sudo -v &> /dev/null
+    # https://gist.github.com/cowboy/3118588
+		while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+		echo "Done!"
+	fi
+}
+
+skip_questions() {
+	while :; do
+		case $1 in
+			-y|--yes) return 0;;
+				*) break;;
+		esac
+		shift 1
+	done
+	return 1
+}
+
+ask_for_confirmation() {
+    echo_warn "$1 (y/n)"
+    read -r -n 1
+    printf "\n"
+}
+
+answer_is_yes() {	[[ "$REPLY" =~ ^[Yy]$ ]] && return 0 || return 1 }
+
+ask() {
+    print_question "$1"
+    read -r
+}
+
+get_answer() { printf "%s" "$REPLY" }
